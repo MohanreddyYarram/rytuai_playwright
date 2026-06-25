fc3ed841138050ef616af598caf79e6de4e1ade5
MohanreddyYarram <yarrammohanreddy@gmail.com>
2026-06-25 22:06:34 +0530

    Added github actions

---
+++ pages/auth/LoginPage.ts
@@ -0,0 +1,36 @@
+
+import { Page,Locator} from '@playwright/test'
+
+export class LoginPage{
+    readonly page:Page;
+    readonly phoneInput:Locator;
+    readonly passwordInput:Locator;
+    readonly loginButton:Locator;
+    readonly errorMessage:Locator;
+
+
+    constructor(page:Page){
+        this.page = page;
+        this.phoneInput = this.page.locator('#login-phone');
+        this.passwordInput = this.page.locator('#login-password');
+        this.loginButton = this.page.getByRole('button', {name:'Login →'});
+        this.errorMessage = this.page.locator('#login-error');
+    }
+   
+    async goto(){
+        await this.page.goto('/')
+    }
+
+    async login(phone:string,password:string){
+        await this.phoneInput.fill(phone)
+        await this.passwordInput.fill(password)
+        await this.loginButton.click();
+    }
+
+    async selectLanguage() {
+    const langBtn = this.page.getByRole('button', { name: 'English' });
+       if (await langBtn.isVisible()) {
+          await langBtn.click();
+         }
+    }
+}  
